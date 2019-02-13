62cfa702a01b075fa0462032b2af52fab3ce9b04
Stephan Girod <24672636+StephGit@users.noreply.github.com>
2019-02-14 00:33:53 +0100

    commented out georef-specific-stuff

--- app/src/main/java/ch/bfh/mad/eazytime/geofence/GeoFenceFragment.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/geofence/GeoFenceFragment.kt
@@ -1,8 +1,6 @@
 package ch.bfh.mad.eazytime.geofence
 
 
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
@@ -12,7 +10,6 @@ import android.view.ViewGroup
 import android.widget.ListView
 import android.widget.ProgressBar
 import ch.bfh.mad.R
-import ch.bfh.mad.eazytime.util.ViewModelFactory
 
 class GeoFenceFragment : GeoFenceBaseFragment() {
 
@@ -45,14 +42,14 @@ class GeoFenceFragment : GeoFenceBaseFragment() {
     }
 
     private fun getListItems(view: View) {
-        val factory = ViewModelFactory()
-        val viewModel: GeoFenceViewModel = ViewModelProviders.of(this, factory).get(GeoFenceViewModel::class.java)
-
-        viewModel.geoFenceItems.observe(this, Observer {
-            val lvGeofences = view.findViewById<ListView>(R.id.lv_geofences)
-            val customAdapter = GeoFenceAdapter(requireContext(), 0, it!!)
-            lvGeofences.adapter = customAdapter
-        })
+//        val factory = ViewModelFactory()
+//        val viewModel: GeoFenceViewModel = ViewModelProviders.of(this, factory).get(GeoFenceViewModel::class.java)
+//
+//        viewModel.geoFenceItems.observe(this, Observer {
+//            val lvGeofences = view.findViewById<ListView>(R.id.lv_geofences)
+//            val customAdapter = GeoFenceAdapter(requireContext(), 0, it!!)
+//            lvGeofences.adapter = customAdapter
+//        })
     }
 
     private fun showEmptyGeofenceFragment() {
