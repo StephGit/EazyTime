74a3a4a578f6b303b31cd1124e7d6a682251caa2
Stephan Girod <24672636+StephGit@users.noreply.github.com>
2019-02-14 00:22:50 +0100

    Update GeoFenceFragment.kt
    added base class

--- app/src/main/java/ch/bfh/mad/eazytime/geofence/GeoFenceFragment.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/geofence/GeoFenceFragment.kt
@@ -1,5 +1,8 @@
 package ch.bfh.mad.eazytime.geofence
 
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
@@ -7,35 +10,60 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
+import android.widget.ProgressBar
 import ch.bfh.mad.R
+import ch.bfh.mad.eazytime.util.ViewModelFactory
 
-class GeoFenceFragment : Fragment() {
+class GeoFenceFragment : GeoFenceBaseFragment() {
 
     private lateinit var listView: ListView
+    private lateinit var progressBar: ProgressBar
+
+    companion object {
+        fun newFragment(): Fragment = GeoFenceFragment()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_geofence, container, false)
         activity!!.title = getString(R.string.geofence_fragment_title)
 
-        listView = view.findViewById<ListView>(R.id.lv_geofences)
+        view.findViewById<FloatingActionButton>(R.id.btn_addGeofence).setOnClickListener { super.addGeofence() }
 
-        view.findViewById<FloatingActionButton>(R.id.btn_addGeofence).setOnClickListener {
-            addGeofence()
-        }
+        listView = view.findViewById(R.id.lv_geofences)
+        progressBar = view.findViewById(R.id.progressBar)
 
-        if (listView.count == 0) showEmptyGeofenceFragment()
+        getListItems(view)
+
+        if (listView.count != 0) showEmptyGeofenceFragment() else showList()
 
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        super.checkPermission(this)
+    }
+
+    private fun getListItems(view: View) {
+        val factory = ViewModelFactory()
+        val viewModel: GeoFenceViewModel = ViewModelProviders.of(this, factory).get(GeoFenceViewModel::class.java)
+
+        viewModel.geoFenceItems.observe(this, Observer {
+            val lvGeofences = view.findViewById<ListView>(R.id.lv_geofences)
+            val customAdapter = GeoFenceAdapter(requireContext(), 0, it!!)
+            lvGeofences.adapter = customAdapter
+        })
+    }
+
     private fun showEmptyGeofenceFragment() {
         activity!!.supportFragmentManager.beginTransaction()
-            .replace(R.id.frame_content, GeoFenceFragmentEmpty())
+            .replace(R.id.frame_content, GeoFenceEmptyFragment.newFragment())
             .addToBackStack(null)
             .commit()
     }
 
-    private fun addGeofence() {
-        // TODO replace with addFragment
+    private fun showList() {
+        progressBar.visibility = View.GONE
+        listView.visibility = View.VISIBLE
     }
 }
