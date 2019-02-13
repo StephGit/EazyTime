9f32a1ee1f7c5bd7b909dff36fe83477201f1496
Stephan Girod <24672636+StephGit@users.noreply.github.com>
2019-02-14 00:23:28 +0100

    Update and rename GeoFenceFragmentEmpty.kt to GeoFenceEmptyFragment.kt
    added base class

--- app/src/main/java/ch/bfh/mad/eazytime/geofence/GeoFenceEmptyFragment.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/geofence/GeoFenceEmptyFragment.kt
@@ -8,22 +8,23 @@ import android.view.View
 import android.view.ViewGroup
 import ch.bfh.mad.R
 
-class GeoFenceFragmentEmpty : Fragment() {
+class GeoFenceEmptyFragment : GeoFenceBaseFragment() {
+
+    companion object {
+        fun newFragment(): Fragment = GeoFenceEmptyFragment()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_geofence_empty, container, false)
         activity!!.title = getString(R.string.geofence_fragment_title)
 
-        view.findViewById<FloatingActionButton>(R.id.btn_addGeofence).setOnClickListener {
-            addGeofence()
-        }
-
+        view.findViewById<FloatingActionButton>(R.id.btn_addGeofence).setOnClickListener { super.addGeofence() }
         return view
     }
 
-    private fun addGeofence() {
-        // TODO replace with addFragment
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        super.checkPermission(this)
     }
-
 
 }
