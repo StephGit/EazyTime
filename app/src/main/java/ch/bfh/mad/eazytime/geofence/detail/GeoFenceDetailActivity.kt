f03b09df0b7ed7e03787b08e21691c09144e3074
StephGit <mail@stephangirod.ch>
2019-02-17 23:42:15 +0100

    added basic map and location functions

--- app/src/main/java/ch/bfh/mad/eazytime/geofence/detail/GeoFenceDetailActivity.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/geofence/detail/GeoFenceDetailActivity.kt
@@ -1,23 +1,47 @@
 package ch.bfh.mad.eazytime.geofence.detail
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import ch.bfh.mad.R
 import ch.bfh.mad.eazytime.EazyTimeActivity
-import com.google.android.gms.maps.*
+import ch.bfh.mad.eazytime.TAG
+import ch.bfh.mad.eazytime.data.entity.GeoFence
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.GeofencingClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 
 
-class GeoFenceDetailActivity : AppCompatActivity(), GeoFenceFlow, OnMapReadyCallback {
+class GeoFenceDetailActivity : AppCompatActivity(),
+    GeoFenceFlow,
+    OnMapReadyCallback,
+    GoogleMap.OnMyLocationButtonClickListener,
+    GoogleMap.OnMyLocationClickListener, GoogleMap.OnMapLongClickListener {
+
     private val defaultZoom: Float = 15F
 
+    private var hasTarget: Boolean = false
+    private var hasGeoFence: Boolean = false
+    private lateinit var geoFence: GeoFence
+
     private lateinit var map: GoogleMap
+    private lateinit var locationProviderClient: FusedLocationProviderClient
+    private lateinit var geofencingClient: GeofencingClient
 
     companion object{
         fun newIntent(ctx: Context)= Intent(ctx, GeoFenceDetailActivity::class.java)
@@ -30,8 +54,16 @@ class GeoFenceDetailActivity : AppCompatActivity(), GeoFenceFlow, OnMapReadyCall
         val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        // TODO how to navigate directly to markerFragement?
-        replaceFragment(GeoFenceDetailFragment.newFragment())
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        geofencingClient = LocationServices.getGeofencingClient(this)
+
+        // TODO navigate directly to markerFragement if no item in bundle
+        if (intent.hasExtra("GEOFENCE_ITEM")) {
+            this.geoFence = intent.getSerializableExtra("GEOFENCE_ITEM") as (GeoFence)
+            replaceFragment(GeoFenceDetailFragment.newFragment())
+        } else {
+            replaceFragment(GeoFenceMarkerFragment.newFragment())
+        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -48,8 +80,33 @@ class GeoFenceDetailActivity : AppCompatActivity(), GeoFenceFlow, OnMapReadyCall
     */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        moveCamera(LatLng(47.031182, 7.854871), defaultZoom)
+        map.uiSettings.isMyLocationButtonEnabled = true
+        map.uiSettings.isMapToolbarEnabled = true
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+        enableLocation()
         showGeoFenceOnMap()
+
+        with(map) {
+            setOnMapLongClickListener(this@GeoFenceDetailActivity)
+        }
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun onMyLocationButtonClick(): Boolean {
+        showCurrentLocation()
+        return true
+    }
+
+    override fun onMapLongClick(p0: LatLng?) {
+        map.addMarker(
+            MarkerOptions()
+                .position(LatLng(p0!!.latitude, p0!!.longitude))
+        )
     }
 
     private fun showGeoFenceOnMap() {
@@ -75,6 +132,43 @@ class GeoFenceDetailActivity : AppCompatActivity(), GeoFenceFlow, OnMapReadyCall
             CameraUpdateFactory.newLatLngZoom(latLng, zoom)
         )
     }
+
+
+    /** Location
+     *
+     */
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocation() {
+        with(map) {
+
+            isMyLocationEnabled = true
+            uiSettings.isMyLocationButtonEnabled = true
+            setOnMyLocationButtonClickListener(this@GeoFenceDetailActivity)
+            setOnMyLocationClickListener(this@GeoFenceDetailActivity)
+        }
+    }
+
+    private fun showCurrentLocation() {
+        try {
+
+            var locationResult: Task<Location> = locationProviderClient.lastLocation
+
+            locationResult.addOnCompleteListener {
+                if (it.isComplete && it.isSuccessful) {
+                    val currentLocation = it.result
+                    currentLocation?.let { loc ->
+                        moveCamera(LatLng(loc.latitude, loc.longitude), defaultZoom)
+                    }
+                }
+            }
+
+        } catch (e: SecurityException) {
+            Log.e(TAG, "Exception: " + e.message)
+        }
+    }
+
+
 
     override fun goToMarker() {
         replaceFragment(GeoFenceMarkerFragment.newFragment())
