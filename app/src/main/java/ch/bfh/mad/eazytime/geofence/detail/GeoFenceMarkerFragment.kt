ed4ba24258a9c40765931c1b166cbaa0dd6d88b2
StephGit <mail@stephangirod.ch>
2019-02-17 23:38:12 +0100

    added layouts and strings for geofenceflow

--- app/src/main/java/ch/bfh/mad/eazytime/geofence/detail/GeoFenceMarkerFragment.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/geofence/detail/GeoFenceMarkerFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import ch.bfh.mad.R
 
 class GeoFenceMarkerFragment : Fragment() {
@@ -23,6 +24,10 @@ class GeoFenceMarkerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_geofence_marker, container, false)
+
+        val proceedButton: Button = view.findViewById(R.id.btn_geoFenceMarkerProceed)
+
+        proceedButton.setOnClickListener { callback.goToRadius() }
 
         return view
     }
--- app/src/main/java/ch/bfh/mad/eazytime/geofence/detail/GeoFenceRadiusFragment.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/geofence/detail/GeoFenceRadiusFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import ch.bfh.mad.R
 
 class GeoFenceRadiusFragment : Fragment() {
@@ -23,6 +24,12 @@ class GeoFenceRadiusFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_geofence_radius, container, false)
+
+        val proceedButton: Button = view.findViewById(R.id.btn_geoFenceRadiusProceed)
+        val backButton: Button = view.findViewById(R.id.btn_geoFenceRadiusBack)
+
+        proceedButton.setOnClickListener { callback.goToDetail() }
+        backButton.setOnClickListener { callback.goToMarker() }
 
         return view
     }
