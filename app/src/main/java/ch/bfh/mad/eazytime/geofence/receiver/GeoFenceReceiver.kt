7e60f0c4c7c5de783c2dd018f66aed57da201480
StephGit <mail@stephangirod.ch>
2019-03-08 20:21:32 +0100

    fixed geofence registration and added async handling to receivers

--- app/src/main/java/ch/bfh/mad/eazytime/geofence/receiver/GeoFenceReceiver.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/geofence/receiver/GeoFenceReceiver.kt
@@ -3,6 +3,7 @@ package ch.bfh.mad.eazytime.geofence.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.util.Log
 import ch.bfh.mad.eazytime.TAG
 import ch.bfh.mad.eazytime.di.Injector
@@ -15,44 +16,69 @@ import javax.inject.Inject
 
 class GeoFenceReceiver : BroadcastReceiver() {
 
-    @Inject
-    lateinit var notificationHandler: NotificationHandler
-
-    @Inject
-    lateinit var timerService: TimerService
-
-    init {
-        Injector.appComponent.inject(this)
-    }
-
-
-//    private val geofenceAction: String = "ch.eazytime.geofence.ACTION_RECEIVE_GEOFENCE"
-
     override fun onReceive(context: Context, intent: Intent) {
-        val geofencingEvent = GeofencingEvent.fromIntent(intent)
-        if (geofencingEvent.hasError()) {
-            val errorMessage = GeofenceErrorMessages.getErrorString(
-                context,
-                geofencingEvent.errorCode
-            )
-            Log.e(TAG, errorMessage)
-            return
-        }
-        handleEvent(context, geofencingEvent)
+        val pendingResult: PendingResult = goAsync()
+        val asyncTask = GeoFenceReceiverTask(pendingResult, intent)
+        asyncTask.execute()
     }
 
-    private fun handleEvent(context: Context, event: GeofencingEvent) {
-        val triggeringGeofences = event.triggeringGeofences
-        if (event.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-            timerService.checkInDefaultProject()
-            triggeringGeofences.first().toString()
-            Log.d(TAG, "GeoFenceReceiver: checked in default project, Geofence " + triggeringGeofences.first().toString())
-            notificationHandler.sendNotification("GeoFenceReceiver: checked in default project, Geofence " + triggeringGeofences.first().toString())
-        } else if  (event.geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
-            timerService.checkOut()
-            triggeringGeofences.first().toString()
-            Log.d(TAG, "GeoFenceReceiver: checked out, Geofence " + triggeringGeofences.first().toString())
-            notificationHandler.sendNotification("GeoFenceReceiver: checked out, Geofence " + triggeringGeofences.first().toString())
+    class GeoFenceReceiverTask internal constructor(
+        private val pendingResult: PendingResult,
+        private val intent: Intent
+    ) : AsyncTask<Void, Void, Void>() {
+
+        private val geoFenceAction: String = "ch.bfh.mad.eazytime.geofence.ACTION_RECEIVE_GEOFENCE"
+
+        @Inject
+        lateinit var notificationHandler: NotificationHandler
+
+        @Inject
+        lateinit var timerService: TimerService
+
+        @Inject
+        lateinit var geoFenceErrorMessages: GeofenceErrorMessages
+
+        init {
+            Injector.appComponent.inject(this)
+        }
+
+        override fun doInBackground(vararg params: Void?): Void? {
+            if (intent.action == geoFenceAction) {
+                val geofencingEvent = GeofencingEvent.fromIntent(intent)
+                if (geofencingEvent.hasError()) {
+                    val errorMessage = geoFenceErrorMessages.getErrorString(
+                        geofencingEvent.errorCode
+                    )
+                    Log.e(TAG, errorMessage)
+                    return null
+                }
+                handleEvent(geofencingEvent)
+            }
+            return null
+        }
+
+        private fun handleEvent(event: GeofencingEvent) {
+            val triggeringGeofences = event.triggeringGeofences
+            if (event.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                timerService.checkInDefaultProject()
+                triggeringGeofences.first().toString()
+                Log.d(
+                    TAG,
+                    "GeoFenceReceiver: checked in default project, Geofence " + triggeringGeofences.first().toString()
+                )
+                notificationHandler.sendNotification("GeoFenceReceiver: checked in default project, Geofence " + triggeringGeofences.first().toString())
+            } else if (event.geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                timerService.checkOut()
+                triggeringGeofences.first().toString()
+                Log.d(TAG, "GeoFenceReceiver: checked out, Geofence " + triggeringGeofences.first().toString())
+                notificationHandler.sendNotification("GeoFenceReceiver: checked out, Geofence " + triggeringGeofences.first().toString())
+            }
+        }
+
+        override fun onPostExecute(result: Void?) {
+            super.onPostExecute(result)
+            // Must call finish() so the BroadcastReceiver can be recycled.
+            pendingResult.finish()
         }
     }
 }
