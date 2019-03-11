dd2d89e4fe9ca9b144f5b2d4cc11cfebf9b1a023
Dominik Hiltbrunner <nikhilti@hispeed.ch>
2019-03-11 16:56:27 +0100

    projectList todaysTimeslots - moved filter for todays timeSlots to repo/dao

--- app/src/main/java/ch/bfh/mad/eazytime/data/repo/TimeSlotRepo.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/data/repo/TimeSlotRepo.kt
@@ -5,11 +5,19 @@ import androidx.lifecycle.LiveData
 import ch.bfh.mad.eazytime.data.dao.TimeSlotDao
 import ch.bfh.mad.eazytime.data.entity.TimeSlot
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.joda.time.LocalDate
+import org.joda.time.LocalTime
 
 class TimeSlotRepo(private val timeSlotDao: TimeSlotDao) {
 
     val allTimeSlots: LiveData<List<TimeSlot>> = timeSlotDao.getTimeSlots()
+
+    @WorkerThread
+    fun todaysTimeSlots(): LiveData<List<TimeSlot>> = runBlocking {
+        timeSlotDao.getTimeSlotsForDay(LocalDate().toLocalDateTime(LocalTime.MIDNIGHT))
+    }
 
     @WorkerThread
     suspend fun insertAll(timeSlots: List<TimeSlot>) = withContext(Dispatchers.IO) {
