cfe98a6e967c216b82e9612dd1e8d7c22450e9f9
StephGit <mail@stephangirod.ch>
2019-03-13 01:08:12 +0100

    cleanup Service

--- app/src/main/java/ch/bfh/mad/eazytime/geofence/GeoFenceService.kt
+++ app/src/main/java/ch/bfh/mad/eazytime/geofence/GeoFenceService.kt
@@ -15,6 +15,8 @@ import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.GeofencingRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import javax.inject.Inject
 
 
@@ -22,23 +24,24 @@ class GeoFenceService @Inject constructor(
     private val context: Context,
     private val geoFenceRepo: GeoFenceRepo,
     private val geofenceErrorMessages: GeofenceErrorMessages
-) {
+) : OnCompleteListener<Void> {
 
-    private val geoFences: MutableList<GeoFence> = ArrayList()
+    private val geoFenceEntities: MutableList<GeoFence> = ArrayList()
+    private val geofences: MutableList<Geofence> = ArrayList()
     private val geofencingClient: GeofencingClient = LocationServices.getGeofencingClient(context)
+    private var localGeofencePendingIntent: PendingIntent? = null
 
     /**
      * Explicit removal of all active geofences and add them again.
      */
     fun initGeoFences() : Boolean {
-        val tmpGeoFences: MutableList<GeoFence> = ArrayList()
-        tmpGeoFences.addAll(geoFenceRepo.getActiveGeoFences())
-        tmpGeoFences.forEach {
-            add(it,
-                success = { Log.d(TAG, "GeoFence " + it.name + " added successfully") },
-                failure = { err -> Log.d(TAG, "Adding failed for GeoFence [" + it.name + "], Error: " + err) })
+
+        geoFenceEntities.addAll(geoFenceRepo.getActiveGeoFences())
+        geoFenceEntities.forEach {
+            remove(it)
+            add(it)
         }
-        return (geoFences.size > 0)
+        return (geofences.size > 0)
     }
 
     /**
@@ -60,73 +63,65 @@ class GeoFenceService @Inject constructor(
     /**
      * Add Geofence to GeofencingClient
      */
-    fun add(
-        geoFence: GeoFence,
-        success: () -> Unit,
-        failure: (error: String) -> Unit
-    ) {
-        if (isInList(geoFence)) {
-            return
-        } else {
-            this.geoFences.add(geoFence)
-        }
-        val gf = buildGeofence(geoFence.gfId!!, geoFence.latitude!!, geoFence.longitude!!, geoFence.radius!!)
-        if (gf != null &&
+    fun add(geoFence: GeoFence) {
+
+        if (!isInList(geoFence)) this.geoFenceEntities.add(geoFence) else return
+
+        val geofence = buildGeofence(geoFence.gfId!!, geoFence.latitude!!, geoFence.longitude!!, geoFence.radius!!)
+        if (geofence != null &&
             ContextCompat.checkSelfPermission(
                 this.context,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED
         ) {
+            geofences.add(geofence)
             geofencingClient
-                .addGeofences(buildGeofencingRequest(gf), geofencePendingIntent)
-                .addOnSuccessListener {
-                    success()
-                }
-                .addOnFailureListener {
-                    failure(geofenceErrorMessages.getErrorString(it))
-                }
+                .addGeofences(buildGeofencingRequest(geofences), geofencePendingIntent)
+                .addOnCompleteListener(this@GeoFenceService)
         }
+    }
+
+    fun remove(geoFence: GeoFence) {
+        if (isInList(geoFence)) this.geoFenceEntities.remove(geoFence) else return
+
+        geofencingClient
+            .removeGeofences(listOf(geoFence.gfId))
+            .addOnCompleteListener(this@GeoFenceService)
     }
 
     private fun isInList(geoFence: GeoFence): Boolean {
-        return (this.geoFences.find { it == geoFence } != null)
+        return (this.geoFenceEntities.find { it == geoFence } != null)
     }
 
-    fun remove(
-        geoFence: GeoFence,
-        success: () -> Unit,
-        failure: (error: String) -> Unit
-    ) {
-        if (!isInList(geoFence)) {
-            return
+    override fun onComplete(task: Task<Void>) {
+        if (task.isSuccessful) {
+            Log.d(TAG, "GeoFence added successfully")
         } else {
-            this.geoFences.remove(geoFence)
+            Log.d(TAG, "Adding failed for GeoFence, Error: " + geofenceErrorMessages.getErrorString(task.exception!!))
         }
-        geofencingClient
-            .removeGeofences(listOf(geoFence.gfId))
-            .addOnSuccessListener {
-                success()
-            }
-            .addOnFailureListener {
-                failure(geofenceErrorMessages.getErrorString(it))
-            }
     }
 
     /**
      *  Setting the value to 0 indicates that you don’t want to trigger a GEOFENCE_TRANSITION_ENTER event if the device is already inside the geofence
+     *  Adds the list of geofences to be monitored
      */
-    private fun buildGeofencingRequest(geofence: Geofence): GeofencingRequest {
+    private fun buildGeofencingRequest(geofences: MutableList<Geofence>): GeofencingRequest {
         return GeofencingRequest.Builder()
             .setInitialTrigger(0)
-            .addGeofence(geofence)
+            .addGeofences(geofences)
             .build()
     }
 
     /**
-     * PendingIntent to trigger TransitionsIntentService
+     * Returns a PendingIntent with the request to trigger GeoFenceBroadcastReceiver.
+     * Location Service addresses the intent inside this PendingIntent.
+     * With usage of FLAG_UPDATE_CURRENT we reuse the same pending intent when calling this function
      */
-    private val geofencePendingIntent: PendingIntent by lazy {
+    private val geofencePendingIntent: PendingIntent? by lazy {
+        if (localGeofencePendingIntent != null) return@lazy localGeofencePendingIntent
+
         val intent = Intent(context, GeoFenceBroadcastReceiver::class.java)
-        PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        localGeofencePendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        return@lazy localGeofencePendingIntent
     }
 }
